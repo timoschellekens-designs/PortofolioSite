42fd84e93704850e979eb2e26d5f6d564fdf6b8f
SaltyJar <95302440+timoschellekens-designs@users.noreply.github.com>
2025-12-09 16:10:57 +0100

    Create Glide.js

---
+++ Glide.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new Glide('.glide', {
+    type: 'carousel',   // 👈 this makes it a carousel
+    perView: 1,         // how many slides visible at once
+    focusAt: 'center',  // optional: keeps the active slide centered
+    autoplay: 3000      // optional: auto-slide every 3s
+  }).mount();
+});
