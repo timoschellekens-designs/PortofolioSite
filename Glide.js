1eb09c213460bdadd416cf8bf77cfdd4f0e90d18
SaltyJar <95302440+timoschellekens-designs@users.noreply.github.com>
2025-12-12 10:36:17 +0100

    Update Glide.js

--- Glide.js
+++ Glide.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     autoplay: 3000      // optional: auto-slide every 3s
   }).mount();
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Glide('.glide_2', {
+    type: 'carousel',   // 👈 this makes it a carousel
+    perView: 1,         // how many slides visible at once
+    focusAt: 'center',  // optional: keeps the active slide centered
+    autoplay: 3000      // optional: auto-slide every 3s
+  }).mount();
+});
